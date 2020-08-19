d1b187280b9ac52d949dbeab4b210b4858a1402a
沈骏 <shenjuneng@sina.com>
2020-08-19 10:18:44 +0800

    宏定义

--- SJNav/SJNav/SJNav/SJNav.h
+++ SJNav/SJNav/SJNav/SJNav.h
@@ -4,13 +4,26 @@
 //
 //  Created by 沈骏 on 2020/8/13.
 //  Copyright © 2020 沈骏. All rights reserved.
-//  2.1.1
+//  2.1.2
 
 #ifndef SJNav_h
 #define SJNav_h
 
 #define kHeight   [UIScreen mainScreen].bounds.size.height
 #define kWidth  [UIScreen mainScreen].bounds.size.width
+
+#define kStatusHeight \
+({CGFloat sh = 0.0;\
+if (@available(iOS 13.0, *)) {\
+    sh = [[[[[UIApplication sharedApplication] windows] firstObject] windowScene] statusBarManager].statusBarFrame.size.height;\
+} else {\
+    sh = 20;\
+}\
+(sh);})
+
+#define kNaviHeight      (kStatusHeight+44)
+
+#define DefaultItemLeftRightSpace 4
 
 #import "SJBaseNav.h"
 
